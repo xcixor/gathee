ad7aaf8a4255492e1308843de2d4e12fdc5ee815
xcixor <pndungu54@gmail.com>
2019-11-19 16:10:04 +0300

    fix(heroku): remove check for extra static files

--- src/core/core/settings.py
+++ src/core/core/settings.py
@@ -122,10 +122,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 import django_heroku
 django_heroku.settings(locals())

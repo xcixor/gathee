59169bd29c938fd2ccc940b982f97a6cc1c58b99
xcixor <pndungu54@gmail.com>
2019-12-12 19:33:10 +0300

    feature(courses): add functionality for registering courses

--- src/core/subscriptions/models/courses/models.py
+++ src/core/subscriptions/models/courses/models.py
@@ -1,11 +1,19 @@
 """Defines the models for Courses."""
 from django.db import models
+from subscriptions.models.tutors.models import Tutor
 
 
 class Course(models.Model):
+    """Course model."""
     title = models.CharField(max_length=40, null=False, blank=False, unique=True)
+    level = models.CharField(max_length=40, null=True, blank=True)
     description = models.CharField(max_length=500, null=True, blank=True)
     course_image = models.ImageField(null=True, upload_to="images/courses/")
+    price = models.IntegerField(null=True, blank=True)
+    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE, null=False, blank=False)
+
 
     def __str__(self):
-        return self.title
+        if self.level:
+            return "{}: {}".format(self.title, self.level)
+        return "{}: {}".format(self.title, "Beginner")
---
+++ src/core/subscriptions/presentation/courses/courses.py
@@ -0,0 +1,10 @@
+"""Contains views related with courses."""
+from django.shortcuts import render
+from subscriptions.application.courses.courses import courses
+
+def courses_view(request):
+    """Renders the courses page."""
+    context = {
+        'courses': courses()
+    }
+    return render(request, 'courses/courses.html', context)

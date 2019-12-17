022b54365e4cb4cd041ed0f60607aecfe2ecb2e6
xcixor <pndungu54@gmail.com>
2019-12-18 00:42:04 +0300

    another try:

--- release.sh
+++ release.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-echo "$GOOGLE_JSON_CREDENTIALS" > src/core/account.json
+# echo "$GOOGLE_JSON_CREDENTIALS" > src/core/account.json
+echo "${SERVICE_ACCOUNT}" > src/core/account.json
 
 # create_service_account() {
 #     mkdir gs-account
 #     touch gs-account/account.json
-#     echo "${SERVICE_ACCOUNT}" > gs-account/account.json
+    # echo "${SERVICE_ACCOUNT}" > gs-account/account.json
 #     echo "success!"
 # }
 

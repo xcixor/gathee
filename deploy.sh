4de9b8173fa9657847e7e6683636f64255d52ab7
xcixor <pndungu54@gmail.com>
2019-12-17 20:27:55 +0300

    restore account file for writing

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 create_service_account() {
-    ls
-    mkdir gs-account
-    touch gs-account/account.json
     echo "${SERVICE_ACCOUNT}" > gs-account/account.json
     echo "success!"
 }

d6f4f97b9602cd898a0820fcb9f21d72be6666d4
xcixor <pndungu54@gmail.com>
2020-01-10 20:45:27 +0300

    encode integration variables

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -34,15 +34,15 @@ check_branch(){
     elif [[ "$CIRCLE_BRANCH" == 'master' ]]; then
         export ENVIRONMENT="production"
         export DJANGO_SETTINGS_MODULE=core.settings.${ENVIRONMENT}
-        tvars=""
-        while IFS=' ' read -ra ADDR; do
-            for i in "${ADDR[@]}"; do
-                tvars+="$i "
-                tvars+="\n"
-            done
-        done <<< "${PRODUCTION_ENVIRONMENTAL_VARIABLES}"
-        echo -e "$tvars" > ~/gathee-deployment/infrastructure/terraform.tfvars
-        # echo "${PRODUCTION_ENVIRONMENTAL_VARIABLES}" > ~/gathee-deployment/infrastructure/terraform.tfvars
+        # tvars=""
+        # while IFS=' ' read -ra ADDR; do
+        #     for i in "${ADDR[@]}"; do
+        #         tvars+="$i "
+        #         tvars+="\n"
+        #     done
+        # done <<< "${PRODUCTION_ENVIRONMENTAL_VARIABLES}"
+        # echo -e "$tvars" > ~/gathee-deployment/infrastructure/terraform.tfvars
+        echo "${PRODUCTION_ENVIRONMENTAL_VARIABLES}" | base64 --decode > ~/gathee-deployment/infrastructure/terraform.tfvars
     else
         export ENVIRONMENT="staging"
         export DJANGO_SETTINGS_MODULE=core.settings.${ENVIRONMENT}
